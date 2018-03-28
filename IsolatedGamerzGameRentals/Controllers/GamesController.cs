fc6f5b0f6ce82319ec640d0d5cfdee63f94f09a2
Halim Lais <halimlais@hotmail.com>
2018-03-28 14:25:54 +0100

    Asp.Net
    working with video game databases

---
+++ IsolatedGamerzGameRentals/Controllers/GamesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//added librabray
+using IsolatedGamerzGameRentals.Models;
+
+namespace IsolatedGamerzGameRentals.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Random()
+        {
+            var game = new Game() { Name = "Super Mario World" };
+
+            return View(game);
+        }
+    }
+}
